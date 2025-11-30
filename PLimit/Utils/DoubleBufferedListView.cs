52e5d3f9562ff335a4f414a063f61ddc4c4c53df
xeL <xcoding.dev@gmail.com>
2025-11-30 14:53:12 +0200

    created double buffered listview class

---
+++ PLimit/Utils/DoubleBufferedListView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLimit
+{
+    public class DoubleBufferedListView : ListView
+    {
+        public DoubleBufferedListView()
+        {
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+            UpdateStyles();
+        }
+    }
+}
