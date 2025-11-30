bd3bcf99dc210320ce33be1d531a5c7f43d88632
xeL <xcoding.dev@gmail.com>
2025-11-30 12:11:20 +0200

    created watermark class

---
+++ PLimit/Utils/WaterMark.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PLimit.Utils
+{
+    internal static class WaterMark
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        /// <summary>
+        /// Set textbox watermark tex.
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <param name="waterMarkText"></param>
+        public static void SetWatermark(this TextBox textBox, string waterMarkText) =>
+            SendMessage(textBox.Handle, 0x1500 + 1, 0, waterMarkText);
+    }
+}
