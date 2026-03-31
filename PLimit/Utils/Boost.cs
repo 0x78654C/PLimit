a8bb76bd4d2f7303afbd5c20b93dd2a59a3564b8
x_coding <xcoding.dev@gmail.com>
2026-03-31 22:42:35 +0300

    created boost class

---
+++ PLimit/Utils/Boost.cs
@@ -0,0 +1,28 @@
+﻿namespace PLimit.Utils
+{
+    public class Boost
+    {
+        public Boost() { }
+
+
+        /// <summary>
+        /// Set boost for the selected process, then refresh the process list and search for the process again to update the boost status in the list.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="processesListBox"></param>
+        /// <param name="label"></param>
+        /// <param name="searchBox"></param>
+        public void SetBoost(Form from, DoubleBufferedListView processesListBox, Label label, TextBox searchBox, bool isEnable)
+        {
+            var processId = processesListBox.SelectedItems[0].SubItems[1].Text;
+            var setBoost = new ProcessesManage();
+            setBoost.SetBoost(isEnable, int.Parse(processId));
+            from.BeginInvoke(new Action(() =>
+            {
+                var utils = new Utils();
+                utils.RefreshProcessList(from, processesListBox, label);
+                utils.SearchProcess(searchBox, processesListBox);
+            }));
+        }
+    }
+}
