49d3cafb3d6317d611b27ebd8c323079829a270f
x_coding <xcoding.dev@gmail.com>
2026-03-31 22:44:48 +0300

    Created IO class for IO management

---
+++ PLimit/Utils/IOPriority.cs
@@ -0,0 +1,87 @@
+﻿namespace PLimit.Utils
+{
+    public class IOPriority
+    {   
+        public IOPriority() { }
+
+        /// <summary>
+        /// Sets the I/O priority of all threads in the selected process to Very Low.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="processesListBox"></param>
+        /// <param name="label"></param>
+        /// <param name="searchBox"></param>
+        public void IOVeryLowPriority(Form from, DoubleBufferedListView processesListBox, Label label, TextBox searchBox)
+        {
+            var processId = processesListBox.SelectedItems[0].SubItems[1].Text;
+            var setIoPriority = new ProcessesManage();
+            setIoPriority.SetIoPriorityAllThreads(int.Parse(processId), ProcessesManage.IO_PRIORITY_HINT.VeryLow);
+            from.BeginInvoke(new Action(() =>
+            {
+                var utils = new Utils();
+                utils.RefreshProcessList(from, processesListBox, label);
+                utils.SearchProcess(searchBox, processesListBox);
+            }));
+        }
+
+        /// <summary>
+        /// Sets the I/O priority of all threads in the selected process to Low.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="processesListBox"></param>
+        /// <param name="label"></param>
+        /// <param name="searchBox"></param>
+        public void IOLowPriority(Form from, DoubleBufferedListView processesListBox, Label label, TextBox searchBox)
+        {
+            var processId = processesListBox.SelectedItems[0].SubItems[1].Text;
+            var setIoPriority = new ProcessesManage();
+            setIoPriority.SetIoPriorityAllThreads(int.Parse(processId), ProcessesManage.IO_PRIORITY_HINT.Low);
+            from.BeginInvoke(new Action(() =>
+            {
+                var utils = new Utils();
+                utils.RefreshProcessList(from, processesListBox, label);
+                utils.SearchProcess(searchBox, processesListBox);
+            }));
+        }
+
+        /// <summary>
+        /// Sets the I/O priority of all threads in the selected process to Normal.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="processesListBox"></param>
+        /// <param name="label"></param>
+        /// <param name="searchBox"></param>
+        public void IONormalPriority(Form from, DoubleBufferedListView processesListBox, Label label, TextBox searchBox)
+        {
+            var processId = processesListBox.SelectedItems[0].SubItems[1].Text;
+            var setIoPriority = new ProcessesManage();
+            setIoPriority.SetIoPriorityAllThreads(int.Parse(processId), ProcessesManage.IO_PRIORITY_HINT.Normal);
+            from.BeginInvoke(new Action(() =>
+            {
+                var utils = new Utils();
+                utils.RefreshProcessList(from, processesListBox, label);
+                utils.SearchProcess(searchBox, processesListBox);
+            }));
+        }
+
+        /// <summary>
+        /// Sets the I/O priority of all threads in the selected process to High.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="processesListBox"></param>
+        /// <param name="label"></param>
+        /// <param name="searchBox"></param>
+        public void IOHighPriority(Form from, DoubleBufferedListView processesListBox, Label label, TextBox searchBox)
+        {
+            var processId = processesListBox.SelectedItems[0].SubItems[1].Text;
+            var setIoPriority = new ProcessesManage();
+            setIoPriority.SetIoPriorityAllThreads(int.Parse(processId), ProcessesManage.IO_PRIORITY_HINT.High);
+            from.BeginInvoke(new Action(() =>
+            {
+                var utils = new Utils();
+                utils.RefreshProcessList(from, processesListBox, label);
+                utils.SearchProcess(searchBox, processesListBox);
+            }));
+        }
+    }
+}
