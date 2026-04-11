72023ecbc5b99b0d1210af848bf96c70af69c2e3
xeL <xcoding.dev@gmail.com>
2026-04-11 09:23:31 +0300

    limited to one instance

--- PLimit/Program.cs
+++ PLimit/Program.cs
@@ -2,16 +2,26 @@ namespace PLimit
 {
     internal static class Program
     {
+        static Mutex mutex = new Mutex(true, "plimit@xcoding");
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                Application.Run(new MainForm());
+                mutex.ReleaseMutex();
+                mutex.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Another instance of PLimit is already running.", "PLimit", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
